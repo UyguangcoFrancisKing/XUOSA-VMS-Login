8a3e87adce5b982b60ef97b6a37bceffb65e250a
UyguangcoFrancisKing <20160009778@my.xu.edu.ph>
2026-02-05 01:57:28 +0800

    docker ready base

--- XU_VMSLogin/entrypoint.sh
+++ XU_VMSLogin/entrypoint.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#python manage.py collectstatic --noinput
-#python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8001 --workers 3 XUOSA_EVS.wsgi:application
+echo "Waiting for MySQL at db:3306..."
+while ! nc -z mysqldb 3306; do
+  sleep 1
+done
+echo "MySQL is up!"
+
+mkdir -p /app/staticfiles /app/static
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+python -m gunicorn --bind 0.0.0.0:8001 --workers 3 XU_VMSLogin.wsgi:application
